bd39aaaf61ed5ca7d2aff9860939eca95e2a78fd
tosawat475 <tosawat.j@ku.th>
2024-09-21 18:06:32 +0000

    add logo and menumain

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,8 @@ const Header = ({ cartCount, toggleCart }) => (
   <header>
     <div className="header-container">
       <div className="profile">
-        <img src="/profile.jpg" alt="Profile" />
-        <span>Hey Max!</span>
+        <img src="https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg" alt="Profile" />
+        <span>Welcome to Resturant</span>
       </div>
       <input type="text" id="searchInput" placeholder="Search your meal..." />
       <div className="cart">
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,8 +5,56 @@ import Cart from './Cart';
 
 const menuItems = [
   { name: 'สปาเกตตี้แฮมเห็ด', price: 150, image: 'https://inwfile.com/s-cy/8avqkv.jpg' },
-  // Add more items as needed
+  { name: 'กะเพราะหมูกรอบ', price: 80, image: 'https://i.ytimg.com/vi/16Odh9KFPK0/maxresdefault.jpg' },
+  { name: 'ข้าวขาหมูเยอรมัน', price: 100, image: 'https://img.wongnai.com/p/400x0/2019/06/14/5f9da9d4fb2446548bb7aeed0ff688c2.jpg' },
+  { name: 'ข้าวหน้าเนื้อ', price: 120, image: 'https://api2.krua.co/wp-content/uploads/2022/07/RI1765_Gallery_-01.jpg' },
+  { name: 'เสต๊กเนื้อ', price: 100, image: 'https://www.calforlife.com/image/food/Beefsteak.jpg' },
 ];
+
+const menumain = [
+    { name: 'กะเพราะหมูกรอบ', price: 80, image: 'https://i.ytimg.com/vi/16Odh9KFPK0/maxresdefault.jpg' },
+    { name: 'หมี่กรอบผัดซีอิ๊ว', price: 60, image: 'https://img.wongnai.com/p/l/2017/11/27/de6df13f67284c978aca21e3a0c52218.jpg' },
+    { name: 'ก๋วยเตี๋ยวคั่วไก่', price: 70, image: 'https://s3-ap-southeast-1.amazonaws.com/photo.wongnai.com/photos/2017/03/29/a3b7af34297c416e90e9c1694ffd206c.jpg' },
+    { name: 'สุกี้แห้งไก่', price: 60, image: 'https://img.wongnai.com/p/l/2017/05/29/6988f264122945718442c9e258babdcb.jpg' },
+    { name: 'ข้าวหมูแดง', price: 60, image: 'https://img.wongnai.com/p/1920x0/2018/07/03/ed7ed753c9f5433fa1afa1a3bfb83796.jpg' },
+    { name: 'ข้าวหมูแดงหมูกรอบ', price: 75, image: 'https://static.thairath.co.th/media/dFQROr7oWzulq5FZX9z21BD6BkebjldCkR4f9gV00c9oK5ObfveJSI516A5C0jFDXvC.webp' },
+    { name: 'ข้าวขาหมูเยอรมัน', price: 100, image: 'https://img.wongnai.com/p/400x0/2019/06/14/5f9da9d4fb2446548bb7aeed0ff688c2.jpg' },
+    { name: 'ข้าวหน้าเนื้อ', price: 120, image: 'https://api2.krua.co/wp-content/uploads/2022/07/RI1765_Gallery_-01.jpg' },
+    { name: 'เสต๊กเนื้อ', price: 100, image: 'https://www.calforlife.com/image/food/Beefsteak.jpg' },
+    { name: 'ข้าวผัดอเมริกัน', price: 115, image: 'https://img.wongnai.com/p/l/2017/05/29/8516e11c77244de6a5f6ac74faa138c5.jpg' },
+    { name: 'ข้าวหน้าหมูทอด', price: 65, image: 'https://img.wongnai.com/p/1920x0/2018/05/25/bc59677defba440b9d6a4507b903df65.jpg' },
+    { name: 'ข้าวยำไก่แซ่บ', price: 65, image: 'https://img.wongnai.com/p/1920x0/2020/11/07/95ce4ec6055a4e02a650c1c58dd41849.jpg' },
+    { name: 'หอยทอด', price: 75, image: 'https://img.wongnai.com/p/l/2017/05/29/5309d0a9b15d4907b6389b6a6787fc7e.jpg' },
+    { name: 'ข้าวน้ำพริกกะปิปลาทู', price: 75, image: 'https://img.wongnai.com/p/1920x0/2018/03/01/a47f84a3a653472cb86d4874ac22e2fe.jpg' },
+    { name: 'ข้าวมันไก่', price: 65, image: 'https://img.wongnai.com/p/l/2017/10/01/ced420b47a6b426d866896d40080b1ca.jpg' },
+];
+
+const menusnack =
+[
+    { name: 'นักเก็ต', price: 45, image: 'https://cdn8.devgodigit.net/wp-content/uploads/2021/09/30191243/051052534_F.jpg' },
+    { name: 'นักเก็ต', price: 45, image: 'https://cdn8.devgodigit.net/wp-content/uploads/2021/09/30191243/051052534_F.jpg' },
+    { name: 'นักเก็ต', price: 45, image: 'https://cdn8.devgodigit.net/wp-content/uploads/2021/09/30191243/051052534_F.jpg' },
+    { name: 'นักเก็ต', price: 45, image: 'https://cdn8.devgodigit.net/wp-content/uploads/2021/09/30191243/051052534_F.jpg' },
+    { name: 'นักเก็ต', price: 45, image: 'https://cdn8.devgodigit.net/wp-content/uploads/2021/09/30191243/051052534_F.jpg' },
+];
+
+const menudrink =
+[
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+   { name: 'น้ำเก๊กฮวย', price: 45, image: 'https://f.ptcdn.info/343/079/000/rn73ea11ekAkF7q758J9-o.jpg' },
+]
 
 const HomePage = () => {
   const [cart, setCart] = useState([]);
@@ -31,11 +79,38 @@ const HomePage = () => {
   return (
     <div>
       <Header cartCount={cart.length} toggleCart={toggleCart} />
+      <nav>
+        <ul>
+            <li><a href="#menu-sction">เมนูแนะนำ</a></li>
+            <li><a href="#main-dishes">อาหารจานเดี่ยว</a></li>
+            <li><a href="#snacks">ของทานเล่น</a></li>
+            <li><a href="#drinks">น้ำ</a></li>
+        </ul>
+        </nav>
       <main>
         <section className="menu-section">
           <h2>เมนูแนะนำ</h2>
           <div className="menu-items">
             {menuItems.map((item, index) => (
+              <MenuItem key={index} item={item} addToCart={addToCart} />
+            ))}
+          </div>
+          <h2>อาหารจานเดี่ยว</h2>
+          <div className="menu-items">
+            {menumain.map((item, index) => (
+              <MenuItem key={index} item={item} addToCart={addToCart} />
+            ))}
+            
+          </div>
+          <h2>ของทานเล่น</h2>
+          <div className="menu-items">
+            {menusnack.map((item, index) => (
+              <MenuItem key={index} item={item} addToCart={addToCart} />
+            ))}
+          </div>
+          <h2>น้ำ</h2>
+          <div className="menu-items">
+            {menudrink.map((item, index) => (
               <MenuItem key={index} item={item} addToCart={addToCart} />
             ))}
           </div>
