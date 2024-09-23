a492c07d1c9f28cb74af421025c1f9fe75d34485
tosawat475 <tosawat.j@ku.th>
2024-09-23 19:08:53 +0000

    edit text search

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Header = ({ cartCount, toggleCart }) => (
+const Header = ({ cartCount, toggleCart,backmain }) => (
   <header>
     <div className="header-container">
       <div className="profile">
         <img src="https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg" alt="Profile" />
-        <span>Welcome to Resturant</span>
+        <span onClick={backmain}>Welcome to Resturant</span>
       </div>
-      <input type="text" id="searchInput" placeholder="Search your meal..." />
+      <input type="text" id="searchInput" placeholder="ค้นหาเมนู..."  />
+      
       <div className="cart">
         <button id="cartButton" onClick={toggleCart}>ตะกร้า (<span id="cartCount">{cartCount}</span>)</button>
       </div>
